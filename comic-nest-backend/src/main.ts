d472f8afbb35ab9a35d14df6c34fa69ee5bcdaaf
w3villa-chinam-patra <chinam.patra@w3villa.com>
2025-07-08 13:48:37 +0530

    feat: integrate Winston logging with NestJS and add logger configuration

--- comic-nest-backend/src/main.ts
+++ comic-nest-backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WinstonModule } from 'nest-winston';
+import { winstonLoggerOptions } from './logger/winston.logger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger(winstonLoggerOptions),
+  });
+
+  const PORT = process.env.PORT ?? 3000;
+  await app.listen(PORT);
+  const logger = new Logger('Bootstrap')
+  logger.log(`Server started at http://localhost:${PORT}`)
 }
 bootstrap();
